b345f075d2001cc3694dcebf68fec2bb27c55772
Manaal Syed <msyed@wpi.edu>
2021-12-26 15:12:28 -0500

    add date module

--- app.js
+++ app.js
@@ -2,24 +2,18 @@
 
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const date = require(`${__dirname}/date.js`)
 const app = express()
-let workItems = []
-let items = []
+
+const workItems = []
+const items = []
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    let today = new Date()
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    let day = today.toLocaleDateString("en-US", options)
-
+    const day = date.getDate()
     res.render("list", { listTitle: day, newListItems: items })
 })
 
@@ -29,7 +23,7 @@ app.get("/work", (req, res) => {
 
 app.post("/", (req, res) => {
     console.log(req.body)
-    let item = req.body.newItem
+    const item = req.body.newItem
     if (req.body.list === "Work") {
         workItems.push(item)
         res.redirect("/work")
@@ -46,11 +40,11 @@ app.get("/about", (req, res) => {
 })
 
 app.post("/work", (req, res) => {
-    let workItem = req.body.newItem
+    const workItem = req.body.newItem
 })
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("server started on port 3000")
 })
---
+++ date.js
@@ -0,0 +1,16 @@
+const today = new Date()
+exports.getDate = function () {
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    return today.toLocaleDateString("en-US", options)
+}
+
+exports.getDay = () => {
+    const options = {
+        weekday: "long"
+    }
+    return today.toLocaleDateString("en-US", options)
+}
